aeb09e653b2babc128bfb68e1a188440d9b43d4e
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-05-20 19:50:14 +0000

    all specs passing

--- spec/controllers/secrets_controller_spec.rb
+++ spec/controllers/secrets_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SecretsController do
       user = create(:person)
       session['cas'] = { 'user' => user.name }
     }
-    
+
     context 'unauthorized' do
       it 'renders 401 status' do
         get :show
@@ -20,7 +20,7 @@ RSpec.describe SecretsController do
         get :show
         expect(response.status).to eq(200)
       end
-      it 'works with a helper method and renders 200' do
+      it 'works with a `:let` method and renders 200' do
         set_current_user
         get :show
         expect(response.status).to eq(200)
