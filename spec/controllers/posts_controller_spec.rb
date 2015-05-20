0870993eee7a09820feb5299fd2a85a769e02dba
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-05-20 19:46:05 +0000

    basic routing spec for home page

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,9 +1,30 @@
 require 'rails_helper'
-RSpec.describe PostsController, :type => :controller do
-  describe '#index' do
-    it 'returns 200 status' do
-      get :index
-      expect(response.status).to eq(200)
+RSpec.describe PostsController do
+  describe 'GET #index' do
+    let(:set_current_user) { 
+      user = create(:person)
+      session['cas'] = { 'user' => user.name }
+    }
+    
+    context 'unauthorized' do
+      it 'renders 401 status' do
+        get :index
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'authorization' do
+      it 'works manually and renders 200' do
+        user = create(:person)
+        session['cas'] = { 'user' => user.name }
+        get :index
+        expect(response.status).to eq(200)
+      end
+      it 'works with a helper method and renders 200' do
+        set_current_user
+        get :index
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
